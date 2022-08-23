04ce40823520b28659ffc96999e8c94687678650
Pablo Baleztena <pablo@ultharsoftware.com>
2022-08-23 17:52:35 -0300

    Clase 44

---
+++ 44-react/dashboard/src/App.jsx
@@ -0,0 +1,66 @@
+import "./App.css";
+
+import BigCard from "./components/BigCard";
+import MiniCard from "./components/MiniCard";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div id="wrapper">
+      {/* <!-- Sidebar --> */}
+      <Sidebar />
+      {/* <!-- End of Sidebar --> */}
+
+      {/* <!-- Content Wrapper --> */}
+      <div id="content-wrapper" className="d-flex flex-column">
+        {/* <!-- Main Content --> */}
+        <div id="content">
+          {/* <!-- Content Row Top --> */}
+          <div className="container-fluid pt-5">
+            <div className="d-sm-flex align-items-center justify-content-between mb-4">
+              <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
+            </div>
+
+            {/* <!-- Content Row Movies--> */}
+            <div className="row">
+              {/* <!-- Movies in Data Base --> */}
+              <MiniCard />
+
+              {/* <!-- Total awards --> */}
+              <MiniCard />
+
+              {/* <!-- Actors quantity --> */}
+              <MiniCard />
+            </div>
+            {/* <!-- End movies in Data Base --> */}
+
+            {/* <!-- Content Row Last Movie in Data Base --> */}
+            <div className="row">
+              {/* <!-- Last Movie in DB --> */}
+              <BigCard />
+              {/* <!-- End content row last movie in Data Base --> */}
+
+              {/* <!-- Genres in DB --> */}
+              <BigCard />
+            </div>
+          </div>
+          {/* <!--End Content Row Top--> */}
+        </div>
+        {/* <!-- End of MainContent --> */}
+
+        {/* <!-- Footer --> */}
+        <footer className="sticky-footer bg-white">
+          <div className="container my-auto">
+            <div className="copyright text-center my-auto">
+              <span>Copyright &copy; Dashboard 2021</span>
+            </div>
+          </div>
+        </footer>
+        {/* <!-- End of Footer --> */}
+      </div>
+      {/* <!-- End of Content Wrapper --> */}
+    </div>
+  );
+}
+
+export default App;
